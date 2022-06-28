123808d34c0f7b1da57ccc7014bc5600d15cc782
CassianoCardoso <dj_kassiano@hotmail.com>
2022-06-28 20:41:15 -0300

    Exercicio Polimorfismo, Herança

---
+++ Adicional/Entities/Employee.cs
@@ -0,0 +1,27 @@
+﻿
+
+namespace Adicional.Entities
+{
+    internal class Employee
+    {
+        public string Name { get; set; }
+        public int Houer  { get; set; }
+        public double ValuePerHouer { get; set; }
+
+        public Employee()
+        {
+        }
+
+        public Employee(string name, int houer, double valuePerHouer)
+        {
+            Name = name;
+            Houer = houer;
+            ValuePerHouer = valuePerHouer;
+        }
+
+        public virtual double Payment() //libera para ser sobreescrita"virtual"
+        {
+            return Houer * ValuePerHouer;
+        }
+    }
+}
---
+++ Adicional/Entities/outsorcedEmproyee.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace Adicional.Entities
+{
+    internal class outsorcedEmproyee : Employee
+    {
+
+        public double AdditionalCharge { get; set; }
+
+        public outsorcedEmproyee()
+        {
+        }
+
+        public outsorcedEmproyee(string name, int houer, double valuePerHouer, double additionalCharge) : base(name, houer, valuePerHouer)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + 1.1 * AdditionalCharge;
+        }
+    }
+}
